f47688236f5da78b356b3570d098fd93a9b1413c
Alien <40964633+Eve124i318074658073416@users.noreply.github.com>
2022-07-29 16:55:26 +0100

    Section 1 done

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,7 +1,7 @@
 import "./ExpenseDate.css";
 
 
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-GB", { month: "long" });
   const year = props.date.toLocaleString("en-GB", { year: "numeric" });
   const day = props.date.toLocaleString("en-GB", { day: "2-digit" });
